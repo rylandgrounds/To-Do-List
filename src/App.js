f05e31f1af407d3d007d6851e140d3451aaeef33
VonLos <rylandgrounds@gmail.com>
2020-12-05 14:07:32 -0600

    added the ability to toggle the theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
 import ToDoList from "./components/ToDoList";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-      <Navbar />
-      <ToDoList />
-      </ThemeContextProvider>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar />
+          <ToDoList />
+          <ThemeToggle />
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
