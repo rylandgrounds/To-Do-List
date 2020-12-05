5f53e5742c48369a6e09d31579560959a6580577
VonLos <rylandgrounds@gmail.com>
2020-12-05 11:45:28 -0600

    added early navbar and list components

---
+++ src/components/ToDoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ToDoList extends Component {
+  render() {
+    return (
+      <div className="to-do-list">
+        <ui>
+          <li>Make the Perfect Cup of Coffee</li>
+          <li>Take Over the World</li>
+          <li>Watch Criminal Minds</li>
+        </ui>
+      </div>
+    );
+  }
+}
+
+export default ToDoList;
